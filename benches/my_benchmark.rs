5bd09adb027710012f7c062efd148e779aab804b
Virgile Andreani <armavica@ulminfo.fr>
2021-07-31 01:08:17 +0200

    Add two benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -98,5 +98,76 @@ fn dimers2(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, sir, macro_sir, dimers, macro_dimers, dimers2, macro_dimers2);
+fn macro_mm(c: &mut Criterion) {
+    define_system! {
+        MM { E, S, ES, P }
+        r_forward : E, S => ES      @ 0.0017
+        r_backward: ES   => E, S    @ 0.5
+        r_cat     : ES   => P       @ 0.1
+    }
+    c.bench_function("macro_mm", |b| {
+        b.iter(|| {
+            let mut mm = MM::new();
+            mm.E = 301;
+            mm.S = 120;
+            mm.advance_until(100.);
+        })
+    });
+}
+
+fn macro_vilar(c: &mut Criterion) {
+    const alphaA: f64 = 50.;
+    const alphapA: f64 = 500.;
+    const alphaR: f64 = 0.01;
+    const alphapR: f64 = 50.;
+    const betaA: f64 = 50.;
+    const betaR: f64 = 5.;
+    const deltaMA: f64 = 10.;
+    const deltaMR: f64 = 0.5;
+    const deltaA: f64 = 1.;
+    const deltaR: f64 = 0.2;
+    const gammaA: f64 = 1.;
+    const gammaR: f64 = 1.;
+    const gammaC: f64 = 2.;
+    const thetaA: f64 = 50.;
+    const thetaR: f64 = 100.;
+    define_system! {
+        Vilar { Da, Dr, Dpa, Dpr, Ma, Mr, A, R, C }
+        r_activation_a      : Da, A => Dpa      @ gammaA
+        r_activation_r      : Dr, A => Dpr      @ gammaR
+        r_deactivation_a    : Dpa   => Da, A    @ thetaA
+        r_deactivation_r    : Dpr   => Dr, A    @ thetaR
+        r_transcription_a   : Da    => Da, Ma   @ alphaA
+        r_transcription_r   : Dr    => Dr, Mr   @ alphaR
+        r_transcription_p_a : Dpa   => Dpa, Ma  @ alphapA
+        r_transcription_p_r : Dpr   => Dpr, Mr  @ alphapR
+        r_translation_a     : Ma    => Ma, A    @ betaA
+        r_translation_r     : Mr    => Mr, R    @ betaR
+        r_complexation      : A, R  => C        @ gammaC
+        r_decomplexation    : C     => R        @ deltaA
+        r_decay_mRNA_a      : Ma    =>          @ deltaMA
+        r_decay_mRNA_r      : Mr    =>          @ deltaMR
+        r_decay_prot_a      : A     =>          @ deltaA
+        r_decay_prot_r      : R     =>          @ deltaR
+    }
+    c.bench_function("macro_vilar", |b| {
+        b.iter(|| {
+            let mut vilar = Vilar::new();
+            vilar.Da = 1;
+            vilar.Dr = 1;
+            vilar.A = 10;
+            vilar.R = 10;
+            vilar.C = 10;
+            vilar.advance_until(200.);
+        })
+    });
+}
+
+criterion_group!(benches,
+    sir, macro_sir,
+    dimers, macro_dimers,
+    dimers2, macro_dimers2,
+    macro_mm,
+    macro_vilar,
+    );
 criterion_main!(benches);

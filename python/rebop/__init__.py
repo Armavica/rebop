8810dcfab13f92c1683cabd540830489377fec05
Virgile Andreani <armavica@ulminfo.fr>
2023-07-15 18:34:31 -0400

    Return results with xarray

---
+++ python/rebop/__init__.py
@@ -0,0 +1,22 @@
+import xarray as xr
+from .rebop import Gillespie
+
+og_run = Gillespie.run
+
+def run_xarray(self: Gillespie, init: dict[str, int], tmax: float, nb_steps: int) -> xr.Dataset:
+    """
+    Run the system until `tmax` with `nb_steps` steps.
+
+    The initial configuration is specified in the dictionary `init`.
+    Returns an xarray Dataset.
+    """
+    times, result = og_run(self, init, tmax, nb_steps)
+    df = xr.Dataset(
+        data_vars={
+            name: xr.DataArray(values, dims="time", coords={"time": times})
+            for name, values in result.items()
+        }
+    )
+    return df
+
+Gillespie.run = run_xarray

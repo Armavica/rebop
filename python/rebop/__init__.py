6bbb221ef7de993ff463310defff8b9a5cf04a9b
Virgile Andreani <armavica@ulminfo.fr>
2023-11-09 20:49:38 -0500

    WIP Make it possible to specify a seed from Python

--- python/rebop/__init__.py
+++ python/rebop/__init__.py
@@ -3,14 +3,14 @@ from .rebop import Gillespie, __version__
 
 og_run = Gillespie.run
 
-def run_xarray(self: Gillespie, init: dict[str, int], tmax: float, nb_steps: int) -> xr.Dataset:
+def run_xarray(self: Gillespie, init: dict[str, int], tmax: float, nb_steps: int, seed: int | None) -> xr.Dataset:
     """
     Run the system until `tmax` with `nb_steps` steps.
 
     The initial configuration is specified in the dictionary `init`.
     Returns an xarray Dataset.
     """
-    times, result = og_run(self, init, tmax, nb_steps)
+    times, result = og_run(self, init, tmax, nb_steps, seed)
     df = xr.Dataset(
         data_vars={
             name: xr.DataArray(values, dims="time", coords={"time": times})
